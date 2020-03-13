9719740511e29d06171118806cb268333cae2292
divyakarthik33 <38841564+divyakarthik33@users.noreply.github.com>
2020-03-13 16:53:01 -0700

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,8 @@
+
+
+document.getElementById("TIME_VAR").innerHTML = "19:03 PM";
+document.getElementById("AIR_VAR").innerHTML = "32 %";
+document.getElementById("SOIL_VAR").innerHTML = "42.2 %";
+document.getElementById("LIGHT_VAR").innerHTML = "2032 lx";
+document.getElementById("TEMP_VAR").innerHTML = "70 \xB0 C"; 
+document.getElementById("TIME_VAR").innerHTML = "03/11/2020, 04:36:33"; 
